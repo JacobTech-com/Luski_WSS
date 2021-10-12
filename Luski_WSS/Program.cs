ea54181fadcc110f90ec6a79bc99c4628b13cf5a
jacob guin <technogeekntraining@gmail.com>
2021-10-11 23:20:32 -0400

    Add project files.

---
+++ Luski_WSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Luski_WSS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
